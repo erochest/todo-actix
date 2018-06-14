708d7a3db151a2b12ebde2cc9071b2b75527f460
Eric Rochester <erochest@gmail.com>
2018-06-14 11:52:22 -0400

    Read the port from $PORT. Fixes #4.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ extern crate actix_web;
 
 use actix_web::http::{Method, StatusCode};
 use actix_web::{server, App, HttpRequest, HttpResponse, Path, Responder, Result};
+use std::env;
 
 fn index(mut _req: HttpRequest) -> Result<HttpResponse> {
-    Ok(HttpResponse::build(StatusCode::OK)
-        .content_type("text/plain")
-        .body("Hello, world!"))
+    Ok(
+        HttpResponse::build(StatusCode::OK)
+            .content_type("text/plain")
+            .body("Hello, world!"),
+    )
 }
 
 fn greeting(info: Path<(u32, String)>) -> impl Responder {
@@ -15,8 +18,12 @@ fn greeting(info: Path<(u32, String)>) -> impl Responder {
 }
 
 fn main() {
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(8080usize);
     let sys = actix::System::new("todo-actix");
-    let addr = "127.0.0.1:8080";
+    let addr = format!("127.0.0.1:{}", &port);
 
     server::new(|| {
         App::new()
