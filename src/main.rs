1f6d8d14fe72110143ab559f31bcca1792a97536
Eric Rochester <erochest@gmail.com>
2018-06-14 13:57:23 -0400

    CORS

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate actix;
 extern crate actix_web;
 
-use actix_web::http::{Method, StatusCode};
 use actix_web::{server, App, HttpRequest, HttpResponse, Path, Responder, Result};
+use actix_web::http::{header, Method, StatusCode};
+use actix_web::middleware::cors;
 use std::env;
 
 fn index(mut _req: HttpRequest) -> Result<HttpResponse> {
@@ -25,15 +26,25 @@ fn main() {
     let sys = actix::System::new("todo-actix");
     let addr = format!("0.0.0.0:{}", &port);
 
-    server::new(|| {
-        App::new()
-            .route("/greeting/{id}/{name}/", Method::GET, greeting)
-            .resource("/", |r| r.f(index))
-    }).bind(&addr)
+    server::new(build_app)
+        .bind(&addr)
         .expect(&format!("Cannot bind to {}", &addr))
         .shutdown_timeout(0)
         .start();
 
     println!("Starting http server: {}", &addr);
     let _ = sys.run();
+}
+
+fn build_app() -> App {
+    App::new()
+        .configure(|app| {
+            cors::Cors::for_app(app)
+                .allowed_origin("*")
+                .allowed_header(header::CONTENT_TYPE)
+                .max_age(3600)
+                .resource("/greeting/{id}/{name}/", |r| r.method(Method::GET).with(greeting))
+                .resource("/", |r| r.f(index))
+                .register()
+        })
 }
