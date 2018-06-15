8c154adb8247a68ff5ce84e312c73deeaccca7c5
Eric Rochester <erochest@gmail.com>
2018-06-15 05:15:14 -0400

    Added state to store todos.
    * Renamed `Message` to `Todo`;
    * `TodoCollection`;
    * `GET /` returns the list of todos;
    * `POST /` adds a todo and returns it;
    * `DELETE /` clears all todos.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,50 @@
 extern crate actix;
 extern crate actix_web;
+extern crate futures;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
-use actix_web::{server, App, HttpRequest, HttpResponse, Json, Path, Responder, Result};
 use actix_web::http::{header, Method};
 use actix_web::middleware::cors;
+use actix_web::{server, App, HttpRequest, HttpResponse, Json, Path, Responder, Result};
 use std::env;
+use std::sync::{Arc, RwLock};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Message {
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct Todo {
     title: String,
 }
 
-fn index(mut _req: HttpRequest) -> Result<HttpResponse> {
-    let messages: Vec<Message> = vec![];
-    Ok(HttpResponse::Ok().json(messages))
+struct TodoCollection {
+    pub todos: Arc<RwLock<Vec<Todo>>>,
 }
 
-fn post_message(input: Json<Message>) -> impl Responder {
-    HttpResponse::Ok().json(input.0)
+impl TodoCollection {
+    fn new(todos: Arc<RwLock<Vec<Todo>>>) -> TodoCollection {
+        TodoCollection { todos }
+    }
 }
 
-fn delete_message(mut _req: HttpRequest) -> Result<HttpResponse> {
+fn index(req: HttpRequest<TodoCollection>) -> Result<HttpResponse> {
+    let todos = Arc::clone(&req.state().todos);
+    let todos = todos.read().unwrap();
+    Ok(HttpResponse::Ok().json(&*todos))
+}
+
+fn post_index((todo, req): (Json<Todo>, HttpRequest<TodoCollection>)) -> Result<HttpResponse> {
+    let todos = Arc::clone(&req.state().todos);
+    {
+        let mut todos = todos.write().unwrap();
+        todos.push(todo.0.clone());
+    }
+    Ok(HttpResponse::Ok().json(todo.0))
+}
+
+fn delete_index(req: HttpRequest<TodoCollection>) -> Result<HttpResponse> {
+    let todos = Arc::clone(&req.state().todos);
+    let mut todos = todos.write().unwrap();
+    todos.clear();
     Ok(HttpResponse::Ok().finish())
 }
 
@@ -38,8 +59,10 @@ fn main() {
         .unwrap_or(8080);
     let sys = actix::System::new("todo-actix");
     let addr = format!("0.0.0.0:{}", &port);
+    let todos = Arc::new(RwLock::new(Vec::new()));
+    let todos_cloned = Arc::clone(&todos);
 
-    server::new(build_app)
+    server::new(move || build_app(Arc::clone(&todos_cloned)))
         .bind(&addr)
         .expect(&format!("Cannot bind to {}", &addr))
         .shutdown_timeout(0)
@@ -49,21 +72,22 @@ fn main() {
     let _ = sys.run();
 }
 
-fn build_app() -> App {
-    App::new()
-        .configure(|app| {
-            cors::Cors::for_app(app)
-                .allowed_origin("https://www.todobackend.com")
-                .allowed_methods(vec!["GET", "POST", "DELETE"])
-                .allowed_headers(vec![header::CONTENT_TYPE])
-                .allowed_header(header::CONTENT_TYPE)
-                .max_age(3600)
-                .resource("/greeting/{id}/{name}/", |r| r.method(Method::GET).with(greeting))
-                .resource("/", |r| {
-                    r.get().f(index);
-                    r.post().with(post_message);
-                    r.delete().f(delete_message);
-                })
-                .register()
-        })
+fn build_app(todos: Arc<RwLock<Vec<Todo>>>) -> App<TodoCollection> {
+    App::with_state(TodoCollection::new(todos)).configure(|app| {
+        cors::Cors::for_app(app)
+            .allowed_origin("https://www.todobackend.com")
+            .allowed_methods(vec!["GET", "POST", "DELETE"])
+            .allowed_headers(vec![header::CONTENT_TYPE])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600)
+            .resource("/greeting/{id}/{name}/", |r| {
+                r.method(Method::GET).with(greeting)
+            })
+            .resource("/", |r| {
+                r.get().with(index);
+                r.post().with(post_index);
+                r.delete().f(delete_index);
+            })
+            .register()
+    })
 }
